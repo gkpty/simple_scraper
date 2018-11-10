6c09d6c6b494b578172444dab9aeb3eef2d3add1
gkpty <gkardonski@gmail.com>
2018-11-10 04:02:10 -0500

    changed to only selenium

--- scraper.py
+++ scraper.py
@@ -13,23 +13,24 @@ driver = webdriver.Chrome(executable_path="/home/fargo/Downloads/chromedriver")
 driver.get(url)
 
 pests = driver.find_elements_by_class_name("ann-box-title")
-size = len(pests)
-
-i=0
-while i < size:
+i = 0
+for pests in pests:
 	posts = driver.find_elements_by_class_name("ann-box-title")
 	posts[i].click()
 	i += 1
-	soup = BeautifulSoup(driver.page_source, "html.parser")
-	for name in soup.findAll(attrs={'class' : 'user-name'}):
-		x = name.text
-		break
-	for phone in soup.findAll(attrs={'class' : 'phone-confirmed'}):
-		a = phone.text
-		b = a.split('V')
-		c = b[0]
-		break
-	print(x, c)
+	item_category = driver.find_element_by_xpath("""/html/body/section[3]/div[4]/div/div/div/ul/li[1]/span[2]""").text
+	pub_date = driver.find_element_by_xpath("""/html/body/section[3]/div[4]/div/div/div/ul/li[2]/span[2]""").text
+	item_location = driver.find_element_by_xpath("""/html/body/section[3]/div[4]/div/div/div/ul/li[3]/span[2]""").text
+	item_price = driver.find_element_by_xpath("""/html/body/section[3]/div[4]/div/div/div/ul/li[4]/span[2]""").text
+	a = driver.find_element_by_xpath("""//*[@id="contactBox"]/div/div[2]/div/div[1]/div[2]/span[1]""").text
+	b = a.split('V')
+	owner_phone = b[0]
+	owner_name = driver.find_element_by_xpath("""//*[@id="contactBox"]/div/div[2]/div/div[1]/div[1]/span[2]""").text
+	print(owner_name, owner_phone, item_category, pub_date, item_location, item_price)
 	driver.back()
 	i += 1
-	continue
+
+nextpage = driver.find_element_by_xpath("""//*[@id="listingview"]/div[4]/div[2]/nav/ul/li[6]/a""")
+nextpage.click()
+
+
